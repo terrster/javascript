12bbfa4f6a2151fc4e20c2ee07a52dbc4807ea9f
terrster <villegasonu@gmail.com>
2021-03-17 18:54:44 -0600

    tipos de datos basicos y variables

--- Practica_1/js/index.js
+++ Practica_1/js/index.js
@@ -1,8 +1,28 @@
-let para = document.getElementById('mainpara')
+let para = document.getElementById('mainpara');
 
+let btnmum = document.getElementById('Numeric');
 
-para.addEventListener('click', WritePara)
+let btntxt = document.getElementById('Text');
 
-function WritePara(){
-    para.textContent = 'Hola Mundo Estoy Vivo'
+let btnbool = document.getElementById('Bool');
+
+btntxt.addEventListener('click', Writetxt);
+
+btnmum.addEventListener('click', WriteNum);
+
+btnbool.addEventListener('click', WriteBool);
+
+function WriteNum(){
+    let numval = 2 + 2;
+    para.textContent = numval;
+}
+
+function Writetxt(){
+    let txtval = "Jonathan Villegas";
+    para.textContent = txtval;
+}
+
+function WriteBool(){
+    let boolvalue = true;
+    para.textContent = boolvalue;
 }
