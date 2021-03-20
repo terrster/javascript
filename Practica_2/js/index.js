c3b110908826ec97e1f65db1bfda85ddb9edc8c3
terrster <villegasonu@gmail.com>
2021-03-19 18:30:40 -0600

    Me equivoque y lo puse en otra practica XD

--- Practica_2/js/index.js
+++ Practica_2/js/index.js
@@ -4,22 +4,69 @@
 
 //Entradas
 
-let btnsuma = document.getElementById('btnsum');
+let suma = document.getElementById('suma');
+suma.addEventListener('click', OpNum1);
 
-btnsuma.addEventListener('click', SumaNum);
+let resta = document.getElementById('resta');
+resta.addEventListener('click', OpNum2);
 
-function SumaNum(){
-    let num1 = Number (document.getElementById('num1').value);
+let multi = document.getElementById('multi');
+multi.addEventListener('click', OpNum3);
 
-    let num2 = Number (document.getElementById('num2').value);
+let divi = document.getElementById('divi');
+divi.addEventListener('click', OpNum4);
 
-    // console.log(num1);
-    // console.log(num2);
 
-    let resultado = num1 + num2;
 
-    let ContenedorResultado = document.getElementById('containerResponse')
+function OpNum1(){
+    let Num1 = Number (document.getElementById('Num1').value);
 
-    ContenedorResultado.innerText = resultado;
+    let Num2 = Number (document.getElementById('Num2').value);
+
+    let resultado = Num1 + Num2;
+
+    let Resultado = document.getElementById('Res')
+
+    Resultado.innerText = resultado;
 }
+
+
+function OpNum2(){
+    let Num1 = Number (document.getElementById('Num1').value);
+
+    let Num2 = Number (document.getElementById('Num2').value);
+
+    let resultado = Num1 - Num2;
+
+    let Resultado = document.getElementById('Res')
+
+    Resultado.innerText = resultado;
+}
+
+
+function OpNum3(){
+    let Num1 = Number (document.getElementById('Num1').value);
+
+    let Num2 = Number (document.getElementById('Num2').value);
+
+    let resultado = Num1 * Num2;
+
+    let Resultado = document.getElementById('Res')
+
+    Resultado.innerText = resultado;
+}
+
+
+function OpNum4(){
+    let Num1 = Number (document.getElementById('Num1').value);
+
+    let Num2 = Number (document.getElementById('Num2').value);
+
+    let resultado = Num1 / Num2;
+
+    let Resultado = document.getElementById('Res')
+
+    Resultado.innerText = resultado;
+}
+
 
