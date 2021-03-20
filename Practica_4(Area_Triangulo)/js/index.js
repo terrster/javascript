2619e6e6527ce18e6c4e4ff8c1ba7c1c1614ce7a
terrster <villegasonu@gmail.com>
2021-03-19 19:33:48 -0600

    Triangulo

---
+++ Practica_4(Area_Triangulo)/js/index.js
@@ -0,0 +1,16 @@
+let calcular = document.getElementById('Calcular');
+calcular.addEventListener ('click',area);
+
+function area(){
+    let alto = Number (document.getElementById('Num1').value);
+    let ancho = Number (document.getElementById('Num2').value);
+
+    let resultado = (alto * ancho)/2;
+
+    let res = document.getElementById('Res');
+
+    res.innerHTML = `
+    <em> El area del triangulo dado es ${resultado} </em>
+    `;
+
+}
