34af6320e9b6d06fef58786579494b5713e095e8
terrster <villegasonu@gmail.com>
2021-04-07 19:45:35 -0500

    Correciones

--- Condicionales_anidadas/Practica_4/js/index.js
+++ Condicionales_anidadas/Practica_4/js/index.js
@@ -9,17 +9,17 @@ function Operacion() {
   if (num1 >= 300) {
     respuesta.innerHTML = 
     `<div class="alert alert-success" role="alert">
-        <strong>El costo del Platillo es de $75</strong>
+        <strong>El costo del Platillo es de $75 por persona dando un total de ${num1*75} pesos</strong>
     </div>`;
   } else if (num1 >=200 && num1 < 300) {
     respuesta.innerHTML = 
     `<div class="alert alert-warning" role="alert">
-        <strong>El costo del Platillo es de $85</strong>
+        <strong>El costo del Platillo es de $85 por persona dando un total de ${num1*85} pesos</strong>
     </div>`;
   } else {
     respuesta.innerHTML = 
     `<div class="alert alert-danger" role="alert">
-        <strong>El costo del Platillo es de $95</strong>
+        <strong>El costo del Platillo es de $95 por persona dando un total de ${num1*95} pesos</strong>
     </div>`;
   }
 }
